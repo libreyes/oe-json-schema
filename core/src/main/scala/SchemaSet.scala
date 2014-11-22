a9a0155df295175cc1f3f52406f2cc9783046f0f
Ben Spencer <ben.spencer@openeyes.org.uk>
2014-11-22 14:01:06 +0000

    Make SchemSet a case class.

--- core/src/main/scala/SchemaSet.scala
+++ core/src/main/scala/SchemaSet.scala
@@ -2,4 +2,4 @@ package uk.org.openeyes.jsonschema.core
 
 import java.net.URI
 
-class SchemaSet[T <: Schema](uriScheme: String, schemas: Map[String, T])
+case class SchemaSet[T <: Schema](uriScheme: String, schemas: Map[String, T])
