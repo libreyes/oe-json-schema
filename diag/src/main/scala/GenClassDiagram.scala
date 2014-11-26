c1306908e2f04c14f43ef464bd51aeb417ad60c6
Ben Spencer <ben.spencer@openeyes.org.uk>
2014-11-26 11:16:08 +0000

    Some rearrangement.

--- diag/src/main/scala/GenClassDiagram.scala
+++ diag/src/main/scala/GenClassDiagram.scala
@@ -16,30 +16,6 @@ object GenClassDiagram {
 
     val schema = new SchemaLoader(Serialization.read[DraftV4Schema](_)).loadFromDir(schemaDir)
 
-    def genLabel(name: String, schema: Schema) = schema match {
-      case schema: DraftV4Schema =>
-        "{" + name + "|" + schema.objectValidation.properties.fold("")(_.foldRight(""){
-          case ((name, schema), acc) => name + ": " + genTypesString(schema) +  "\\l" + acc
-        }) + "}"
-    }
-
-    def genTypesString(schema: Schema) = schema match {
-      case SchemaRef(uri) => uri.getSchemeSpecificPart
-      case schema: DraftV4Schema => schema.generalValidation.types.fold("?")(_.types.map(genTypeString(schema, _)).mkString("\\|"))
-    }
-
-    def genTypeString(schema: DraftV4Schema, t: Type): String = {
-      if (t == TArray) {
-        schema.arrayValidation.items match {
-          case Some(ItemsSchema(itemSchema)) => genTypesString(itemSchema) + " [" + schema.arrayValidation.minItems.getOrElse(0) + ".." + schema.arrayValidation.maxItems.getOrElse("*") + "]"
-          case Some(ItemsSchemaList(itemSchemas)) => throw new Exception("TODO")
-          case _ => throw new Exception("No array validation defined for schema with type array:\n" + Serialization.writePretty(schema))
-        }
-      } else {
-        t.toString
-      }
-    }
-
     val (nodes, inheritEdges) = schema.generalValidation.definitions.getOrElse(Seq()).map {
       case (name, schema) => (Node(name, "label" -> genLabel(name, schema)), schema.parentSchemaRefs.map(uri => Edge(uri.getSchemeSpecificPart -> name)))
     }.toSeq.unzip
@@ -49,5 +25,29 @@ object GenClassDiagram {
     val w = new java.io.FileWriter(outputFile)
     graph.format(w)
     w.close
+  }
+
+  private def genLabel(name: String, schema: Schema) = schema match {
+    case schema: DraftV4Schema =>
+      "{" + name + "|" + schema.objectValidation.properties.fold("")(_.foldRight(""){
+        case ((name, schema), acc) => name + ": " + genTypesString(schema) +  "\\l" + acc
+      }) + "}"
+  }
+
+  private def genTypesString(schema: Schema) = schema match {
+    case SchemaRef(uri) => uri.getSchemeSpecificPart
+    case schema: DraftV4Schema => schema.generalValidation.types.fold("?")(_.types.map(genTypeString(schema, _)).mkString("\\|"))
+  }
+
+  private def genTypeString(schema: DraftV4Schema, t: Type): String = {
+    if (t == TArray) {
+      schema.arrayValidation.items match {
+        case Some(ItemsSchema(itemSchema)) => genTypesString(itemSchema) + " [" + schema.arrayValidation.minItems.getOrElse(0) + ".." + schema.arrayValidation.maxItems.getOrElse("*") + "]"
+        case Some(ItemsSchemaList(itemSchemas)) => throw new Exception("TODO")
+        case _ => throw new Exception("No array validation defined for schema with type array:\n" + Serialization.writePretty(schema))
+      }
+    } else {
+      t.toString
+    }
   }
 }
